56fd8ff805720f4bb41e66ee838f35e783727126
Exaucé Boby Zakuda <97030641+exauceboby@users.noreply.github.com>
2026-07-10 16:02:02 +0300

    Update verified experience metrics

--- portofolio/src/components/Experience/Experience.jsx
+++ portofolio/src/components/Experience/Experience.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
 import './Experience.css'
 
-const Experience = () => {
-  return (
-    <div className='experience' id='Experience'>
-        <div className='achievement'>
-            <div className='circle'>4+</div>
-            <span>years</span>
-            <span>Experience</span>
-        </div>
-        <div className='achievement'>
-            <div className='circle'>10+</div>
-            <span>Completed</span>
-            <span>Projects</span>
-        </div>
-        <div className='achievement'>
-            <div className='circle'>3+</div>
-            <span>Companies</span>
-            <span>Work</span>
-        </div>
+const Experience = () => (
+  <div className='experience' id='Experience'>
+    <div className='achievement'>
+      <div className='circle'>5+</div>
+      <span>Years</span>
+      <span>Experience</span>
     </div>
-  )
-}
+    <div className='achievement'>
+      <div className='circle'>10+</div>
+      <span>Software</span>
+      <span>Projects</span>
+    </div>
+    <div className='achievement'>
+      <div className='circle'>7+</div>
+      <span>Organizations</span>
+      <span>Supported</span>
+    </div>
+  </div>
+)
 
 export default Experience
