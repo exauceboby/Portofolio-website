2e33d7e5ae2b04ed843553fa7b1033223ee3804a
Exaucé Boby Zakuda <97030641+exauceboby@users.noreply.github.com>
2026-07-10 16:01:35 +0300

    Align navigation with professional portfolio sections

--- portofolio/src/components/Navbar/Navbar.jsx
+++ portofolio/src/components/Navbar/Navbar.jsx
@@ -1,39 +1,27 @@
 import React from 'react'
 import './Navbar.css'
 import Toggle from '../Toggle/Toggle'
-import {Link} from 'react-scroll'
+import { Link } from 'react-scroll'
 
-const Navbar = () => {
-  return (
-    <div className='n-wrapper'>
-      <div className='n-left'>
-        <div className='n-name'>Exauce BOBY</div>
-        <Toggle/>
-      </div>
-      <div className='n-right'>
-        <div className='n-list'>
-            <ul style={{listStyleType: 'none'}}>
-              <Link spy={true} to='Navbar' smooth={true} activeClass='activeClass'>
-              <li>Home</li>
-              </Link>
-              <Link spy={true} to='Services' smooth={true} activeClass='activeClass'>
-              <li>Services</li>
-              </Link>
-              <Link spy={true} to='Experience' smooth={true} activeClass='activeClass'>
-              <li>Experience</li>
-              </Link>
-              <Link spy={true} to='Portfolio' smooth={true} activeClass='activeClass'>
-              <li>Portfolio</li>
-              </Link>
-              <Link spy={true} to='Testimonials' smooth={true} activeClass='activeClass'>
-              <li>Testimonials</li>
-              </Link>
-            </ul>
-        </div>
-        <button className='button n-button'>Contact</button>
-      </div>
+const Navbar = () => (
+  <div className='n-wrapper' id='Navbar'>
+    <div className='n-left'>
+      <div className='n-name'>Exaucé Boby Zakuda</div>
+      <Toggle />
     </div>
-  )
-}
+    <div className='n-right'>
+      <div className='n-list'>
+        <ul style={{ listStyleType: 'none' }}>
+          <Link spy to='Intro' smooth activeClass='activeClass'><li>Home</li></Link>
+          <Link spy to='Services' smooth activeClass='activeClass'><li>Services</li></Link>
+          <Link spy to='Experience' smooth activeClass='activeClass'><li>Experience</li></Link>
+          <Link spy to='Portfolio' smooth activeClass='activeClass'><li>Portfolio</li></Link>
+          <Link spy to='Career' smooth activeClass='activeClass'><li>Background</li></Link>
+        </ul>
+      </div>
+      <Link to='Contact' smooth><button className='button n-button'>Contact</button></Link>
+    </div>
+  </div>
+)
 
 export default Navbar
