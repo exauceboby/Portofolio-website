56c26d007c66a87854475b5efb4763a69d070407
Exaucé Boby Zakuda <97030641+exauceboby@users.noreply.github.com>
2026-07-10 16:04:22 +0300

    Update footer with verified contact links

--- portofolio/src/components/Footer/Footer.jsx
+++ portofolio/src/components/Footer/Footer.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
 import './Footer.css'
-import Insta from '@iconscout/react-unicons/icons/uil-instagram'
-import Facebook from '@iconscout/react-unicons/icons/uil-facebook'
+import Linkedin from '@iconscout/react-unicons/icons/uil-linkedin'
 import Github from '@iconscout/react-unicons/icons/uil-github'
+import Envelope from '@iconscout/react-unicons/icons/uil-envelope'
 import Wave from '../../img/wave.png'
 
-const Footer = () => {
-  return (
-    <div className='footer'>
-        <img src={Wave} alt="" style={{width:'100%'}} />
-        <div className='f-content'>
-
-            <a href="mailto:exauceboby3@gmail.com" style={{color:"white"}}><span>exauceboby3@gmail.com</span> </a>
-            <div className='f-icons'>
-            <Insta color='white' size='3rem'/>
-            <Facebook color='white' size='3rem'/>
-            <Github color='white' size='3rem'/>
-        </div>
-        </div>
-      
+const Footer = () => (
+  <div className='footer'>
+    <img src={Wave} alt='' style={{ width: '100%' }} />
+    <div className='f-content'>
+      <a href='mailto:exauceboby15@gmail.com' style={{ color: 'white' }}>
+        <span>exauceboby15@gmail.com</span>
+      </a>
+      <div className='f-icons'>
+        <a href='mailto:exauceboby15@gmail.com' aria-label='Email'><Envelope color='white' size='2.6rem' /></a>
+        <a href='https://www.linkedin.com/in/exauce-boby-2006ba2a4' target='_blank' rel='noreferrer' aria-label='LinkedIn'><Linkedin color='white' size='2.6rem' /></a>
+        <a href='https://github.com/exauceboby' target='_blank' rel='noreferrer' aria-label='GitHub'><Github color='white' size='2.6rem' /></a>
+      </div>
     </div>
-  )
-}
+  </div>
+)
 
 export default Footer
